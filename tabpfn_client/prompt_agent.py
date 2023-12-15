a0eb289328a535d0357c55daed20b374436856e8
Anshul Gupta <anshulg954@Anshuls-MacBook-Air.local>
2023-12-15 19:01:09 +0100

    fix secre tcode

--- tabpfn_client/prompt_agent.py
+++ tabpfn_client/prompt_agent.py
@@ -49,7 +49,7 @@ class PromptAgent:
             password = getpass.getpass(cls.indent(password_req_prompt))
             password_confirm = getpass.getpass(cls.indent("Please confirm your password: "))
 
-            validation_link = input(cls.indent("Please enter your secret code: "))
+            validation_link = "tabpfn-2023" #input(cls.indent("Please enter your secret code: "))
             
             user_auth_handler.set_token_by_registration(email, password, password_confirm, validation_link)
 
