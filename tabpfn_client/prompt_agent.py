5896d187893b3e8b5c30db13c24c8fde140c45df
Anshul Gupta <anshulg954@Anshuls-MacBook-Air.local>
2023-12-15 01:12:36 +0100

    Update: Modified the validation link and updated Server url

--- tabpfn_client/prompt_agent.py
+++ tabpfn_client/prompt_agent.py
@@ -46,7 +46,7 @@ class PromptAgent:
             password = getpass.getpass(cls.indent(password_req_prompt))
             password_confirm = getpass.getpass(cls.indent("Please confirm your password: "))
 
-            validation_link = input(cls.indent("Please enter your validation link: "))
+            validation_link = input(cls.indent("Please enter your secret code: "))
             
             user_auth_handler.set_token_by_registration(email, password, password_confirm, validation_link)
 
