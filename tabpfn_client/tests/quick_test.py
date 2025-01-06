af2d6e4dda23b6963f16b36fc3e3ffd378c88833
LeoGrin <leo.grinsztajn@polytechnique.edu>
2025-01-06 22:14:35 +0100

    move quick_test in tests

--- tabpfn_client/tests/quick_test.py
+++ tabpfn_client/tests/quick_test.py
@@ -1,3 +1,11 @@
+"""
+TabPFN Client Example Usage
+--------------------------
+Toy script to check that the TabPFN client is working.
+Use the breast cancer dataset for classification and the diabetes dataset for regression,
+and try various prediction types.
+"""
+
 import logging
 
 from sklearn.datasets import load_breast_cancer, load_diabetes
@@ -20,9 +28,8 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.33, random_state=42
     )
-    # make y text
-    y_train = y_train.astype(str) + "a"
-    y_test = y_test.astype(str) + "a"
+    y_train = y_train
+    y_test = y_test
 
     tabpfn = TabPFNClassifier(n_estimators=3)
     # print("checking estimator", check_estimator(tabpfn))
@@ -47,4 +54,5 @@ if __name__ == "__main__":
 
     print(UserDataClient.get_data_summary())
     # test predict_full
+    print("predicting ")
     print(tabpfn.predict(X_test[:30], output_type="full", quantiles=[0.1, 0.5, 0.9]))
