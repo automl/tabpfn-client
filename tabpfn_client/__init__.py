f5d621eb24f492b8653a78401fdd09c56ef3863a
noahho <Noah.homa@gmail.com>
2025-01-07 17:46:39 +0100

    - Add Copyright

--- tabpfn_client/__init__.py
+++ tabpfn_client/__init__.py
@@ -1,3 +1,6 @@
+#  Copyright (c) Prior Labs GmbH 2025.
+#  Licensed under the Apache License, Version 2.0
+
 from tabpfn_client.config import init, reset, get_access_token, set_access_token
 from tabpfn_client.estimator import TabPFNClassifier, TabPFNRegressor
 from tabpfn_client.service_wrapper import UserDataClient
